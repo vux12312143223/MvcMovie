f770fcaabbdeb4cbaa9e01a063a8ee85b5cb872d
Vu Pham <13046336-vuxpham@users.noreply.gitlab.com>
2024-06-15 20:44:14 +0700

    Add project files.

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+
+    // List of genres for user to choose from
+    public SelectList? Genres { get; set; }
+
+    // Selected genre
+    public string? MovieGenre { get; set; }
+
+    // Search term
+    public string? SearchString { get; set; }
+}
